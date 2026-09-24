159eaa62511305d061ca2782e223d99dd7ece66a
Marcosgmp <marcosgustavomp2@gmail.com>
2026-09-24 20:31:04 -0300

    feat(domain): adiciona enum TodoStatus

---
+++ src/HavanTestTech.Domain/Enums/TodoStatus.cs
@@ -0,0 +1,9 @@
+namespace HavanTestTech.Domain.Enums;
+
+// The "Todo" prefix avoids a name clash with System.Threading.Tasks.Task.
+public enum TodoStatus
+{
+    Pending,
+    InProgress,
+    Completed
+}
